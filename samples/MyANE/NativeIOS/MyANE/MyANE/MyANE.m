0e6451bcf45ef5ddc469e4592035256d4ac594df
Lloyd Heberlie <lheberlie@esri.com>
2013-03-23 12:21:43 -0500

    Initial code commit of supporting source code of ANE used in the hello-ane sample.  This is the source code that created the ANE used in the hello-ane sample.

---
+++ samples/MyANE/NativeIOS/MyANE/MyANE/MyANE.m
@@ -0,0 +1,155 @@
+//////////////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright 2012 Freshplanet (http://freshplanet.com | opensource@freshplanet.com)
+//  
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//  
+//    http://www.apache.org/licenses/LICENSE-2.0
+//  
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//  
+//////////////////////////////////////////////////////////////////////////////////////
+
+#import "MyANE.h"
+
+FREContext MyANECtx = nil;
+
+
+@implementation MyANE
+
+#pragma mark - Singleton
+
+static MyANE *sharedInstance = nil;
+
++ (MyANE *)sharedInstance
+{
+    if (sharedInstance == nil)
+    {
+        sharedInstance = [[super allocWithZone:NULL] init];
+    }
+
+    return sharedInstance;
+}
+
++ (id)allocWithZone:(NSZone *)zone
+{
+    return [self sharedInstance];
+}
+
+- (id)copy
+{
+    return self;
+}
+
+@end
+
+
+#pragma mark - C interface
+
+/* This is a TEST function that is being included as part of this template. 
+ *
+ * Users of this template are expected to change this and add similar functions 
+ * to be able to call the native functions in the ANE from their ActionScript code
+ */
+DEFINE_ANE_FUNCTION(IsSupported)
+{
+    NSLog(@"Entering IsSupported()");
+
+    FREObject fo;
+
+    FREResult aResult = FRENewObjectFromBool(YES, &fo);
+    if (aResult == FRE_OK)
+    {
+        //things are fine
+        NSLog(@"Result = %d", aResult);
+    }
+    else
+    {
+        //aResult could be FRE_INVALID_ARGUMENT or FRE_WRONG_THREAD, take appropriate action.
+        NSLog(@"Result = %d", aResult);
+    }
+    
+    NSLog(@"Exiting IsSupported()");
+    
+    return fo;
+}
+
+
+#pragma mark - ANE setup
+
+/* MyANEExtInitializer()
+ * The extension initializer is called the first time the ActionScript side of the extension
+ * calls ExtensionContext.createExtensionContext() for any context.
+ *
+ * Please note: this should be same as the <initializer> specified in the extension.xml 
+ */
+void MyANEExtInitializer(void** extDataToSet, FREContextInitializer* ctxInitializerToSet, FREContextFinalizer* ctxFinalizerToSet) 
+{
+    NSLog(@"Entering MyANEExtInitializer()");
+
+    *extDataToSet = NULL;
+    *ctxInitializerToSet = &ContextInitializer;
+    *ctxFinalizerToSet = &ContextFinalizer;
+
+    NSLog(@"Exiting MyANEExtInitializer()");
+}
+
+/* MyANEExtFinalizer()
+ * The extension finalizer is called when the runtime unloads the extension. However, it may not always called.
+ *
+ * Please note: this should be same as the <finalizer> specified in the extension.xml 
+ */
+void MyANEExtFinalizer(void* extData) 
+{
+    NSLog(@"Entering MyANEExtFinalizer()");
+
+    // Nothing to clean up.
+    NSLog(@"Exiting MyANEExtFinalizer()");
+    return;
+}
+
+/* ContextInitializer()
+ * The context initializer is called when the runtime creates the extension context instance.
+ */
+void ContextInitializer(void* extData, const uint8_t* ctxType, FREContext ctx, uint32_t* numFunctionsToTest, const FRENamedFunction** functionsToSet)
+{
+    NSLog(@"Entering ContextInitializer()");
+
+    /* The following code describes the functions that are exposed by this native extension to the ActionScript code.
+     * As a sample, the function isSupported is being provided.
+     */
+    *numFunctionsToTest = 1;
+
+    FRENamedFunction* func = (FRENamedFunction*) malloc(sizeof(FRENamedFunction) * (*numFunctionsToTest));
+    func[0].name = (const uint8_t*) "isSupported";
+    func[0].functionData = NULL;
+    func[0].function = &IsSupported;
+
+    *functionsToSet = func;
+
+    MyANECtx = ctx;
+
+    NSLog(@"Exiting ContextInitializer()");
+}
+
+/* ContextFinalizer()
+ * The context finalizer is called when the extension's ActionScript code
+ * calls the ExtensionContext instance's dispose() method.
+ * If the AIR runtime garbage collector disposes of the ExtensionContext instance, the runtime also calls ContextFinalizer().
+ */
+void ContextFinalizer(FREContext ctx) 
+{
+    NSLog(@"Entering ContextFinalizer()");
+
+    // Nothing to clean up.
+    NSLog(@"Exiting ContextFinalizer()");
+    return;
+}
+
+
